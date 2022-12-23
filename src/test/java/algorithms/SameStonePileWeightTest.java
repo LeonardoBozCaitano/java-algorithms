3523a728cc218c73eedac27a62254cbe1f45dc5f
Leonardo Boz Caitano <leonardoboz@gmail.com>
2022-12-23 14:56:52 -0300

    Adding some tests

--- src/test/java/algorithms/SameStonePileWeightTest.java
+++ src/test/java/algorithms/SameStonePileWeightTest.java
@@ -13,5 +13,8 @@ class SameStonePileWeightTest {
         assertEquals(3, SameStonePileWeight.run(new int[]{5, 8, 13, 27, 14}));
         assertEquals(1, SameStonePileWeight.run(new int[]{5, 8, 13, 27, 14, 1, 1}));
         assertEquals(3, SameStonePileWeight.run(new int[]{1, 2, 3, 9}));
+        assertEquals(0, SameStonePileWeight.run(new int[]{}));
+        assertEquals(5, SameStonePileWeight.run(new int[]{5}));
+        assertEquals(2, SameStonePileWeight.run(new int[]{4, 2}));
     }
 }
